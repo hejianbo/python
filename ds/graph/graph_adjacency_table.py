3e479ba52089783ed27f8badc84f86e9f4ca4c61
hejianbo <hejanbo@qq.com>
2019-11-24 12:35:09 +0800

    实现基于邻接表的深度与广度遍历“

--- ds/graph/graph_adjacency_table.py
+++ ds/graph/graph_adjacency_table.py
@@ -1,4 +1,5 @@
 from ds.graph.edge import Edge
+from queue import Queue
 
 """
 图结点
@@ -41,6 +42,41 @@ class GraphAdjacencyTable(object):
         node.first_edge = adj_node
 
         # 如果是无向图
+
+    def dfs(self, vertex, visited):
+        """
+        深度优化遍历
+        vertex: 顶点索引
+        visited: 表示此顶点是否已经被访问过
+        """
+        visited[vertex] = True
+        # 遍历与该结点相邻的数据
+        node = self.data[vertex]
+        edge = node.first_edge
+        while edge is not None:
+            if not visited[edge.vertex_index]:
+                # 如果没有被访问，则开始递归访问
+                self.dfs(edge.vertex_index, visited)
+            edge = edge.next_edge
+    
+    def bfs(self, vertex, visited):
+        """
+        广度优化: 先访问， 再入队，出队不访问
+                  先入队，出队再访问
+        """
+        queue = Queue()
+        # 先将第一个访问的元素入队
+        queue.put(vertex)
+        while not queue.empty():
+            vertex = queue.get()
+            visited[vertex] = True
+
+            # 将该顶点的邻接点放入到队列中
+            node = self.data[vertex]
+            edge = node.first_edge
+            while edge is not None:
+                if not visited[edge.v2]:
+                    queue.put(edge.v2)
 
 if __name__ == "__main__":
     vertex_number = 10
